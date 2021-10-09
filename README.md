1c68f2888e7d5daa52ef8b576805d58313bcb9c5
Flyspeck101 <lawrence.chen8@education.nsw.gov.au>
2021-10-10 10:02:53 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Keyboard Master Custom Values
  
+This site takes in parameters and gives a URL for Keyboard Master. 
---
+++ index.js
@@ -0,0 +1,51 @@
+let things = {
+    mode: 0,
+    time: 120,
+    pointGoal: 2000,
+    stageTime: 30
+};
+
+let url = "https://split-diamond-labs.github.io/Keyboard-Master?mode=0&time=1200&pointGoal=2000&stageTime=300";
+document.getElementById("url").value = url;
+
+const modes = ["Timed Challenge", "Point Challenge", "Stage By Stage Challenge"]
+
+document.getElementById("mode").innerText = modes[0];
+document.getElementById("time").value = 120;
+document.getElementById("pointGoal").value = 2000;
+document.getElementById("stageTime").value = 30;
+
+let updateURL = () => {
+    url = `https://split-diamond-labs.github.io/Keyboard-Master?mode=${things.mode}&time=${things.time*10}&pointGoal=${things.pointGoal}&stageTime=${things.stageTime*10}`;
+    document.getElementById("url").value = url;
+}
+
+document.getElementById("mode").addEventListener("click", (e) => {
+    things.mode++; // Toggle mode 
+    things.mode %= modes.length; // If the mode index overflows, this line will automatically bring it back to 0
+    e.target.innerText = modes[things.mode];
+    updateURL();
+});
+
+document.getElementById("time").addEventListener("change", (e) => {
+    if (typeof JSON.parse(e.target.value) != "number") return;
+
+    things[e.target.id] = JSON.parse(e.target.value);
+    updateURL();
+});
+
+document.getElementById("pointGoal").addEventListener("change", (e) => {
+    if (typeof JSON.parse(e.target.value) != "number") return;
+
+    things[e.target.id] = JSON.parse(e.target.value);
+    updateURL();
+});
+
+document.getElementById("stageTime").addEventListener("change", (e) => {
+    if (typeof JSON.parse(e.target.value) != "number") return;
+
+    things[e.target.id] = JSON.parse(e.target.value);
+    updateURL();
+});
+
+document.getElementById("url").addEventListener("change", updateURL);
